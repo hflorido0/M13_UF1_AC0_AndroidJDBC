c07c9ff18278d58e994744ea6b22ccca8131a007
yitan <hflorido@correu.escoladeltreball.org>
2022-11-24 20:11:07 +0100

    Initial commit

--- app/src/main/java/edu/uoc/androidjdbc/main/MainActivity.kt
+++ app/src/main/java/edu/uoc/androidjdbc/main/MainActivity.kt
@@ -3,10 +3,13 @@ package edu.uoc.androidjdbc.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import edu.uoc.androidjdbc.R
+import edu.uoc.androidjdbc.controller.Controller
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var controller : Controller = Controller()
+        controller.init()
     }
 }
--- app/src/main/java/edu/uoc/androidjdbc/model/Persona.java
+++ app/src/main/java/edu/uoc/androidjdbc/model/Persona.java
@@ -1,4 +1,28 @@
 package edu.uoc.androidjdbc.model;
 
 public class Persona {
+    private int id;
+    private String DNI;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+
+    public Persona(int id, String DNI, String nombre, String apellidos, String direccion) {
+        this.id = id;
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", DNI='" + DNI + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
 }
